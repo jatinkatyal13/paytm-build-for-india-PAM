915a895e20c0ceb054ebdb3b8dfac0529edc1b47
Jatin Katyal <jatin.katyal13@gmail.com>
2018-04-07 18:21:00 +0530

    DRF initiated

--- api/main/views.py
+++ api/main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
+from .models import *
+from .serializers import *
 
 # Create your views here.
+
+def patients(request):
+	pat = Patient.objects.all()
+	ser = PatientSerializer(pat, many=True)
+	return JsonResponse(ser.data, safe=False)
+
